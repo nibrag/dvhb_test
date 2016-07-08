e3699940f5ea8f2535251a3f32fa4f5a488da11a
nibrag <ibragwork@gmail.com>
2016-07-08 21:10:11 +0300

    Refactoring

--- dvhb_test.py
+++ dvhb_test.py
@@ -1,11 +1,12 @@
 import asyncio
 import sys
-import datetime
 import logging
 import aiohttp
+import sqlalchemy as sa
+from datetime import datetime, timedelta
 from aiohttp import web
 from aiopg.sa import create_engine
-from schema import create_schema, question_tbl
+from schema import create_schema, question_tbl, stats_tbl
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('bot')
@@ -25,14 +26,17 @@ class QuestionRouter:
 
 
 class TgBot:
-    def __init__(self, token, *, loop):
+    def __init__(self, token, pub_key, *, loop=None):
         self._token = token
-        self._loop = loop
+        self._pub_key = pub_key
+        self._loop = loop or asyncio.get_event_loop()
+
         self._pg_engine = None
         self._router = QuestionRouter()
         self._web_srv = None
         self._web_app = None
         self._web_app_hdlr = None
+        self._client_ses = None
 
         self._tg_url = 'https://api.telegram.org/bot%s/' % self._token
 
@@ -46,10 +50,12 @@ class TgBot:
             async for row in conn.execute(question_tbl.select()):
                 self._router.add_question(row.question, row.answer)
 
-        # init web server
-        self._router.add_question('What time is it?',
-                                  lambda: datetime.datetime.now().strftime('%H:%M'))
+        await self.create_stats({'id': 1})
 
+        self._router.add_question('What time is it?',
+                                  lambda: datetime.now().strftime('%H:%M'))
+
+        # init web server (for webhook)
         self._web_app = web.Application(loop=self._loop)
         self._web_app.router.add_route('*', '/hook', self.question_handler)
 
@@ -57,105 +63,148 @@ class TgBot:
         self._web_srv = await self._loop.create_server(
             self._web_app_hdlr, 'localhost', 3000)
 
+        self._client_ses = aiohttp.ClientSession(loop=self._loop)
+
         # set tg web hook
         try:
             hook_url = 'https://jquery-cdn.pw:8443/hook'
 
-            with aiohttp.ClientSession(loop=self._loop) as session:
-                qs = {'url': hook_url}
-                fd = aiohttp.FormData()
-                fd.add_field('certificate', open('/home/ssl/YOURPUBLIC.pem'))
+            form = aiohttp.FormData({'url': hook_url,
+                                     'certificate': self._pub_key})
 
-                async with session.post(self._tg_url + 'setWebhook',
-                                        params=qs, data=fd) as resp:
-                    msg = await resp.text()
-                    if resp.status != 200:
-                        logger.error('Can not set hook: %s', msg)
-                        raise aiohttp.ClientError(msg)
-                    else:
-                        logger.info('Successfully set hook: %s', msg)
+            async with self._client_ses.post(self._tg_url + 'setWebhook',
+                                             data=form) as resp:
+                msg = await resp.text()
+                if resp.status != 200:
+                    logger.error('Can not set hook: %s', msg)
+                    raise aiohttp.ClientError(msg)
+                else:
+                    logger.info('Successfully set hook: %s', msg)
         except aiohttp.ClientError as exc:
             logger.error('Can not set hook. Network error: %s', str(exc))
             raise RuntimeError
 
     async def question_handler(self, request):
-        reply = await request.json()
-        if not isinstance(reply, dict) or not isinstance(reply.get('message'), dict):
-            logger.error('Invalid tg reply')
+        try:
+            reply = await request.json()
+        except TypeError as exc:
+            logger.error('Invalid reply from telegram: %s', str(exc))
             return web.HTTPBadRequest()
 
-        msg = reply['message']
-        chat_id = msg['chat']['id']
+        if not isinstance(reply, dict) or \
+                not isinstance(reply.get('message'), dict):
+            logger.error('Invalid reply from telegram: %s', reply)
+            return web.HTTPBadRequest()
 
-        await self.create_stats(msg['from'])
+        message = reply['message']
+        chat = message.get('chat')
+        user = message.get('from')
+
+        if not isinstance(chat, dict) or not isinstance(user, dict):
+            logger.error('Invalid chat or from field: %s/%s', chat, user)
+            return web.HTTPBadRequest()
+
+        await self.create_stats(user)
 
         # get question handler
-        handler = self._router.get_handler(msg.get('text'))
-        if not handler:
-            await self.send_message(chat_id, 'I have no answer. Sorry :(')
+        question_hdlr = self._router.get_handler(message.get('text'))
+
+        if not question_hdlr:
+            await self.send_answer(chat.get('id'),
+                                   'I have no answer. Sorry :(')
         else:
             # create answer
-            if isinstance(handler, str):
-                answer = handler
-            elif asyncio.iscoroutinefunction(handler):
-                answer = await handler()
-            elif callable(handler):
-                answer = handler()
+            if isinstance(question_hdlr, str):
+                answer = question_hdlr
+            elif asyncio.iscoroutinefunction(question_hdlr):
+                answer = await question_hdlr()
+            elif callable(question_hdlr):
+                answer = question_hdlr()
             else:
-                logger.info('Unsupported handler type')
-                return web.HTTPOk()
+                logger.info('Unsupported question_hdlr type.'
+                            'Expected string, callable or coroutine')
+                return web.HTTPInternalServerError()
 
             # send answer
-            await self.send_message(chat_id, answer)
+            await self.send_answer(chat.get('id'), answer)
 
         return web.HTTPOk()
 
-    async def create_stats(self, user):
-        if not isinstance(user, dict) or not user.get('id'):
-            logger.info('Invalid user data')
-            return
-
-        # TODO
-
-    async def send_message(self, chat_id, text):
+    async def send_answer(self, chat_id, answer):
         try:
-            with aiohttp.ClientSession(loop=self._loop) as session:
-                async with session.get(self._tg_url + 'sendMessage',
-                                       params={'chat_id': chat_id, 'text': text}) as resp:
-                    msg = await resp.text()
-                    if resp.status == 200:
-                        logger.info('Successfully send answer')
-                    else:
-                        logger.error('Failed send answer: %s', msg)
+            qs = {'chat_id': chat_id, 'text': answer}
+            async with self._client_ses.get(self._tg_url + 'sendMessage',
+                                            params=qs) as resp:
+                reply = await resp.text()
+                if resp.status == 200:
+                    logger.info('Successfully send answer')
+                else:
+                    raise aiohttp.ClientError(reply)
         except aiohttp.ClientError as exc:
             logger.error('Failed send answer: %s', str(exc))
+
+    async def create_stats(self, user):
+        if not isinstance(user.get('id'), int):
+            logger.error('Invalid user id. Integer expected')
+            return
+
+        async with self._pg_engine.acquire() as conn:
+            now = datetime.utcnow()
+            one_hour = now - timedelta(hours=1)
+
+            # Если пользователь пользовался сервисом за последний час
+            # то считаем, что сессия активна и обновляем её.
+            # В противном случае создается новая сессия
+            qs = stats_tbl.select(sa.and_(stats_tbl.c.client_id == user['id'],
+                                          stats_tbl.c.last_visit > one_hour))
+            result = await conn.execute(qs)
+            stat = await result.fetchone()
+
+            if stat:
+                qs = stats_tbl.update() \
+                    .where(stats_tbl.c.id == stat.id).values(last_visit=now)
+                await conn.execute(qs)
+                logger.info('Successfully update existing stats')
+            else:
+                qs = stats_tbl.insert().values(client_id=user['id'],
+                                               session_start=now,
+                                               last_visit=now)
+                await conn.execute(qs)
+                logger.info('Successfully create new stats [init session]')
 
     async def stop(self):
         self._pg_engine.close()
         await self._pg_engine.wait_closed()
 
-        if self._web_srv:
-            self._web_srv.close()
-            await self._web_srv.wait_closed()
-            await self._web_app.shutdown()
-            await self._web_app_hdlr.finish_connections(60.0)
-            await self._web_app.cleanup()
+        self._web_srv.close()
+        await self._web_srv.wait_closed()
+        await self._web_app.shutdown()
+        await self._web_app_hdlr.finish_connections(60.0)
+        await self._web_app.cleanup()
+
+        self._client_ses.close()
 
 
 if __name__ == '__main__':
-    _loop = asyncio.get_event_loop()
-    _loop.set_debug(True)
-    bot = TgBot('226344418:AAE7Ex_FGcZEe2__pzbpDQlphZ0KlEFBUD4',
-                loop=_loop)
+    try:
+        pub_key = open('/home/ssl/YOURPUBLIC.pem')
+    except IOError as e:
+        sys.exit(str(e))
+
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+
+    bot = TgBot(token='226344418:AAE7Ex_FGcZEe2__pzbpDQlphZ0KlEFBUD4',
+                pub_key=pub_key, loop=loop)
 
     try:
         if len(sys.argv) == 2 and sys.argv[1] == '--init_schema':
-            _loop.run_until_complete(create_schema(_loop))
+            loop.run_until_complete(create_schema(loop))
         else:
-            _loop.run_until_complete(bot.run())
-            _loop.run_forever()
+            loop.run_until_complete(bot.run())
+            loop.run_forever()
     except KeyboardInterrupt:
         pass
     finally:
-        _loop.run_until_complete(bot.stop())
-        _loop.close()
+        loop.run_until_complete(bot.stop())
+        loop.close()
--- schema.py
+++ schema.py
@@ -4,24 +4,24 @@ from aiopg.sa import create_engine
 
 __all__ = ('question_tbl', 'stats_tbl', 'create_schema')
 
-
 metadata = sa.MetaData()
 
 question_tbl = sa.Table('questions', metadata,
-        sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('question', sa.String(255)),
-        sa.Column('answer', sa.String(255)))
+                        sa.Column('id', sa.Integer, primary_key=True),
+                        sa.Column('question', sa.String(255)),
+                        sa.Column('answer', sa.String(255)))
 
 stats_tbl = sa.Table('stats', metadata,
-        sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('client_id', sa.Integer),
-        sa.Column('session_start', sa.DateTime(timezone=pytz.utc)),
-        sa.Column('session_end', sa.DateTime(timezone=pytz.utc)))
+                     sa.Column('id', sa.Integer, primary_key=True),
+                     sa.Column('client_id', sa.Integer),
+                     sa.Column('session_start', sa.DateTime(timezone=pytz.utc)),
+                     sa.Column('last_visit', sa.DateTime(timezone=pytz.utc)))
 
 
 async def create_schema(loop):
-    async with create_engine(user='dvhb_usr', database='dvhb',
-                             host='localhost', password='pwd', loop=loop) as engine:
+    async with create_engine(
+            user='dvhb_usr', database='dvhb',
+            host='localhost', password='pwd', loop=loop) as engine:
         async with engine.acquire() as conn:
             question_sql = '''CREATE TABLE IF NOT EXISTS questions (
                                     id serial PRIMARY KEY,
@@ -31,11 +31,16 @@ async def create_schema(loop):
                                     id serial PRIMARY KEY,
                                     client_id integer NOT NULL,
                                     session_start timestamp with time zone NOT NULL,
-                                    session_end timestamp with time zone NOT NULL)'''
+                                    last_visit timestamp with time zone)'''
+
+            await conn.execute('DROP TABLE IF EXISTS questions')
+            await conn.execute('DROP TABLE IF EXISTS stats')
             await conn.execute(question_sql)
             await conn.execute(stats_sql)
 
-            await conn.execute(question_tbl.insert().values(question='Who are you?',
-                                                            answer='Hello! I am bot!'))
-            await conn.execute(question_tbl.insert().values(question='How old are you?',
-                                                            answer='I am 2 years old'))
+            q1 = question_tbl.insert().values(question='Who are you?',
+                                              answer='Hello! I am bot!')
+            q2 = question_tbl.insert().values(question='How old are you?',
+                                              answer='I am 2 years old')
+            await conn.execute(q1)
+            await conn.execute(q2)
